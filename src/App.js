6df25d85f3a06733963862d4baf080ee3391425f
Matthew Wessel <nucmatt@gmail.com>
2021-01-27 11:29:55 -0600

    Style: Complete intial styling for basic html layout

--- src/App.js
+++ src/App.js
@@ -1,36 +1,57 @@
 import './scss/style.scss';
 
 function App() {
-  return (
-    <div className="container bg-success">
-      <main>
-        <h1>Pomodoro Clock</h1>
-        <div id="timer">
-          <h2 id="timer-label">Session</h2>
-          <div id="time-left">25:00</div>
-        </div>
-        <div id="controls">
-          <button id="start_stop">Start/Pause</button>
-          <button id="reset">Reset</button>
-        </div>
-        <div id="time-lengths">
-          <div id="session">
-            <h3 id="session-label">Session Length</h3>
-            <button id="session-increment">+</button>
-            <span id="session-length">25</span>
-            <button id="session-decrement">-</button>
-          </div>
-          <div id="break">
-            <h2 id="break-label">Break Length</h2>
-            <button id="break-increment">+</button>
-            <span id="break-length">5</span>
-            <button id="break-decrement">-</button>
-          </div>
-        </div>
-      </main>
-      
-    </div>
-  );
+	return (
+		<div className='container bg-dark'>
+			<main>
+				<h1>Pomodoro Clock</h1>
+				<div id='timer'>
+					<h2 id='timer-label'>Session</h2>
+					<div id='time-left' className='x-large'>
+						25:00
+					</div>
+				</div>
+				<div id='controls'>
+					<button id='start_stop' className='btn'>
+						Start/Pause
+					</button>
+					<button id='reset' className='btn'>
+						Reset
+					</button>
+				</div>
+				<div id='time-lengths'>
+					<div id='session' className='m-1'>
+						<h3 id='session-label'>Session Length</h3>
+						<div className='timer-adjust'>
+							<button id='session-increment' className='btn'>
+								+
+							</button>
+							<span id='session-length' className='large'>
+								25
+							</span>
+							<button id='session-decrement' className='btn'>
+								-
+							</button>
+						</div>
+					</div>
+					<div id='break' className='m-1'>
+						<h3 id='break-label'>Break Length</h3>
+						<div className='timer-adjust'>
+							<button id='break-increment' className='btn'>
+								+
+							</button>
+							<span id='break-length' className='large'>
+								5
+							</span>
+							<button id='break-decrement' className='btn'>
+								-
+							</button>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
