6eaece8801931e29dc60acd6c423af42a522b768
Matthew Wessel <nucmatt@gmail.com>
2021-02-03 10:05:17 -0600

    Build: Add countdown timer functionality
    Adds the ability for the timer to count down. The useRef and useEffect
    hooks are used to create this countdown effect without constant
    rerenders when setRemainingTime changes.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './scss/style.scss';
 
 function App() {
 	const [breakLength, setBreakLength] = useState(5);
 	const [sessionLength, setSessionLength] = useState(25);
-	const [remainingTime, setRemainingTime] = useState(sessionLength)
+	const [remainingTime, setRemainingTime] = useState(sessionLength * 60);
 	const [isRunning, setIsRunning] = useState(false);
 
+	let intervalRef = useRef();
+	const countDown = () => {
+		setRemainingTime((prev) => prev - 1);
+	};
+	useEffect(() => {
+		if (isRunning) {
+			intervalRef.current = setInterval(countDown, 1000);
+			return () => clearInterval(intervalRef.current);
+		}
+	}, [isRunning]);
 	const handleIncrement = (e) => {
 		if (e.target.id === 'session-increment' && sessionLength <= 59) {
 			setSessionLength(sessionLength + 1);
 		} else if (e.target.id === 'break-increment' && breakLength <= 59) {
-			setBreakLength(breakLength + 1)
+			setBreakLength(breakLength + 1);
 		} else {
-			return
+			return;
 		}
-	}
+	};
 	const handleDecrement = (e) => {
 		if (e.target.id === 'session-decrement' && sessionLength > 1) {
 			setSessionLength(sessionLength - 1);
 		} else if (e.target.id === 'break-decrement' && breakLength > 1) {
-			setBreakLength(breakLength - 1)
+			setBreakLength(breakLength - 1);
 		} else {
-			return
+			return;
 		}
-	}
+	};
 	const handleTimer = () => {
 		return setIsRunning(!isRunning);
-	}
+	};
 
-	const displayTime = (length) => {
-		let timeInSeconds = length * 60;
+	const displayTime = (timeInSeconds) => {
 		let minutes = Math.floor(timeInSeconds / 60);
-		let seconds = timeInSeconds - (minutes * 60);
-		return `${minutes}:${seconds < 10 ? '0' + seconds : seconds}` ;
-	}
+		let seconds = timeInSeconds - minutes * 60;
+		return `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+	};
 
 	return (
 		<div className='container bg-dark'>
@@ -58,13 +67,21 @@ function App() {
 					<div id='session' className='m-1'>
 						<h3 id='session-label'>Session Length</h3>
 						<div className='timer-adjust'>
-							<button id='session-increment' className='btn' onClick={handleIncrement}>
+							<button
+								id='session-increment'
+								className='btn'
+								onClick={handleIncrement}
+							>
 								+
 							</button>
 							<span id='session-length' className='large'>
 								{sessionLength}
 							</span>
-							<button id='session-decrement' className='btn' onClick={handleDecrement}>
+							<button
+								id='session-decrement'
+								className='btn'
+								onClick={handleDecrement}
+							>
 								-
 							</button>
 						</div>
@@ -72,13 +89,21 @@ function App() {
 					<div id='break' className='m-1'>
 						<h3 id='break-label'>Break Length</h3>
 						<div className='timer-adjust'>
-							<button id='break-increment' className='btn' onClick={handleIncrement}>
+							<button
+								id='break-increment'
+								className='btn'
+								onClick={handleIncrement}
+							>
 								+
 							</button>
 							<span id='break-length' className='large'>
 								{breakLength}
 							</span>
-							<button id='break-decrement' className='btn' onClick={handleDecrement}>
+							<button
+								id='break-decrement'
+								className='btn'
+								onClick={handleDecrement}
+							>
 								-
 							</button>
 						</div>
